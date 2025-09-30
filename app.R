0158b0cd064bd30985ab83468cd55472d4c2efe9
Paripurna Bawonoputro <col.flamealchemist@gmail.com>
2025-09-30 16:44:26 +0200

    app revision

--- app.R
+++ app.R
@@ -1,8 +1,20 @@
+# app.R
+
 if (!requireNamespace("Lab5", quietly = TRUE)) {
   if (!requireNamespace("remotes", quietly = TRUE)) install.packages("remotes")
   remotes::install_github("bawonoputro/Lab5")
 }
+
 library(shiny)
 library(Lab5)
+
+ui <- fluidPage(
+  titlePanel("ShinyKolada"),
+  tableOutput("mun")
+)
+
+server <- function(input, output, session) {
+  output$mun <- renderTable(head(get_municipalities(), 6))
+}
 
 shinyApp(ui, server)
